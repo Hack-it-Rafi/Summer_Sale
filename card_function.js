a7aaf9c5c29de3c8e729d8dd8bbcec50d6a1de7b
Hack-it-Rafi <zozorafi2016@gmail.com>
2023-08-20 13:17:05 +0600

    Finally Done

--- card_function.js
+++ card_function.js
@@ -1,23 +1,21 @@
 function cardFunction(data){
-    // console.log(data.childNodes[3].childNodes[3].innerText);
     const itemName = data.childNodes[3].childNodes[3].innerText;
-    // console.log(itemName);
     const addElement = document.getElementById('addHere');
     const count = addElement.childElementCount;
     const p = document.createElement('p');
     p.innerHTML = `${count+1} ${itemName}`
-    if(count+1<10){
+    // if(count+1<10){
         addElement.appendChild(p);
-    }
+    // }
     const textPrice = data.childNodes[3].childNodes[5].childNodes[1].childNodes[0].innerText;
     const price = parseFloat(textPrice);
 
     const totalPriceElement = document.getElementById('total_price');
     const textTotalPrice = totalPriceElement.innerText;
     totalPrice = parseFloat(textTotalPrice);
-    if(count+1<10){
+    // if(count+1<10){
         totalPrice += price;
-    }
+    // }
 
     totalPrice = totalPrice.toFixed(2);
     totalPriceElement.innerText = totalPrice;
@@ -32,3 +30,6 @@ function cardFunction(data){
         document.getElementById('purchaseButton').removeAttribute('disabled');
     }
 }
+document.getElementById('purchaseButton').addEventListener('click', function(){
+    document.getElementById('purchaseButton').setAttribute('disabled', true);
+})
